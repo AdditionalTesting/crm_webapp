3b1b3eb783617c47ad244fad29968427d8b1e3c1
AlB <albert.burc@gmail.com>
2017-07-12 20:11:08 -0400

    contacts working

--- crm.rb
+++ crm.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
-require_relative 'contact'
+require_relative 'contact.rb'
 
 get '/' do
   erb :index
+end
+
+get '/contacts' do
+  @list_of_contacts = Contact.all
+  erb :contacts
+end
+
+
+
+
+
+
+
+
+after do
+  ActiveRecord::Base.connection.close
 end
